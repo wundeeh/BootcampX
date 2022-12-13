e61b0342e5be26445b7372c2e1d5165b160233d2
wundeeh <jakiriksh@gmail.com>
2022-12-13 21:36:11 +0000

    show assignments with most assistance requests

---
+++ 4_queries/10_confusing_assignments.sql
@@ -0,0 +1,6 @@
+SELECT assignments.id as id, assignments.name as name, assignments.day as day, 
+assignments.chapter as chapter, COUNT(assistance_requests.assignment_id) as total_requests
+FROM assistance_requests
+JOIN assignments ON assignments.id = assistance_requests.assignment_id
+GROUP BY assignments.id
+ORDER BY total_requests DESC;
